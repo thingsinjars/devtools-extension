2d76e6bc04d5c34231138ca7b4fd33566aadce00
Simon Madine <simon@angryrobotzombie.com>
2013-05-27 22:17:56 +0200

    Changed demo name

--- devtools.js
+++ devtools.js
@@ -3,4 +3,4 @@
 // chrome.extension.*
 
 // Create a tab in the devtools area
-chrome.devtools.panels.create("GhostStory", "toast.png", "panel.html", function(panel) {});
+chrome.devtools.panels.create("DemoPanel", "toast.png", "panel.html", function(panel) {});
